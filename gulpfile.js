b5443c1803144238e83cc10e0c85e8b1c3bafd07
Maurof18 <maurof809@gmail.com>
2023-12-06 11:28:08 -0300

    Creando Mixins en SASS

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,13 @@ function css(){
             .pipe( dest('./build/css') )
 }
 
+function minificarcss() {
+    return src('src/scss/app.scss')
+            .pipe(sass({
+                outputStyle: 'compressed'
+            }))
+            .pipe(dest('./build/css'))
+}
 function watchArchivos(){
     watch('src/scss/**/*.scss',css); //! * =  la carpeta actual - ** = todos los archivos con esa extension
 }
