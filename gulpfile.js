25c09153f7e718b1a0e7a58f597eae4f75f52c0e
Maurof18 <maurof809@gmail.com>
2023-12-11 19:22:58 -0300

    Notificando imagenes comprimidas y unificar varias tareas

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 const {series, src, dest,watch}= require('gulp') ;
 const sass = require('gulp-sass')(require('sass'));
-const imagemin= require('gulp-imagemin');
+const imagemin = require('gulp-imagemin');
+const notify = require( 'gulp-notify');
+
+
 function css(){
 // el src es una funcion q encuentra el scss de una ubicacion
    return src('src/scss/app.scss')
             .pipe( sass({
                 outputStyle: 'expanded'
             }) )
-            .pipe( dest('./build/css') )
+            .pipe( dest('./build/css') );
 }
 function imagenes(){
     return src('src/img/**/*')
         .pipe(imagemin())
         .pipe(dest('./build/img'))
+        .pipe(notify({message: 'Imagen Minificada'}));
 }
 
 function minificarcss() {
@@ -28,3 +32,5 @@ function watchArchivos(){
 exports.css= css;
 exports.imagenes = imagenes;
 exports.watchArchivos=watchArchivos; 
+
+exports.default = series(css,imagenes, watchArchivos);
