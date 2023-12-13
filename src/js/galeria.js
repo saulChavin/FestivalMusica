184b9280578878d5a5c5d4090378432789e1e372
Maurof18 <maurof809@gmail.com>
2023-12-13 13:47:22 -0300

    Mostrar una imagen mas grande al dar click

--- src/js/galeria.js
+++ src/js/galeria.js
@@ -8,11 +8,49 @@ function crearGaleria(){
     for(let i=1 ; i<=12 ; i++){
         const imagen= document.createElement('IMG'); //genero la imagen
         imagen.src =`build/img/thumb/${i}.webp`;
-       
+        imagen.dataset.imagenId=i; //permite crear atributos personalizados 
+        //añadir la funcion de mostrar  imagen 
+        imagen.onclick=mostrarImagen; //agrega un evento
         const lista = document.createElement('LI');
         lista.appendChild(imagen);
 
         galeria.appendChild(lista);
-
     }
+}
+
+function mostrarImagen(e){
+    const id= parseInt(e.target.dataset.imagenId);
+
+    //genera la imagen 
+    const imagen= document.createElement('IMG');
+    imagen.src= `build/img/grande/${id}.webp`;
+
+    const overlay=document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+    //cuando se da click se cierra la imagen
+    overlay.onclick=function(){
+        overlay.remove();
+    }
+
+    //botton para cerrar la imagen
+    const cerrarImagen= document.createElement('P');
+    cerrarImagen.textContent='X';
+    cerrarImagen.classList.add('btn-cerrar');
+
+    overlay.appendChild(cerrarImagen);
+
+    //cuando se preciona se cierra la imagen
+    cerrarImagen.onclick= function(){
+        overlay.remove();
+    }
+
+
+    //mostrar en el HTML
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fijar-body');
+
+
 }
