b1a583e277c20d4e76194926f6769e97e0b89829
Maurof18 <maurof809@gmail.com>
2023-12-21 10:08:26 -0300

    Intersection Observer para fijar la navegacion

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,8 +1,26 @@
 document.addEventListener('DOMContentLoaded',function(){
     scrollNav();
+    navegacionFija();
 })
 
+function navegacionFija(){
+    const barra= document.querySelector('.header');
 
+
+    //Registrar el intersection Observer
+    // observer revisa si un elemento esta visible o no en la ventana del Navegador
+    const observer = new IntersectionObserver(function(entries){//entries no da la informacion del elemento a observar
+        if(entries[0].isIntersecting){
+            barra.classList.remove('fijo');
+        }else{
+            barra.classList.add('fijo')
+        }
+    });
+
+    //Elemento a observar
+    observer.observe(document.querySelector('.sobre-festival'));
+
+}
 function scrollNav(){
     const enlaces=document.querySelectorAll('.navegacion-principal a');
 
